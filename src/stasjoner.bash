52bee96d903bb468fec478e4397d9ab7c276ad53
Fred-Olav Mosdal <fred-olav@mosdal.org>
2020-10-12 20:32:08 +0200

    Jobbete med stasjoner

---
+++ src/stasjoner.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# "Concatenating" all selected questions/answers/notes into a single TeX file.
+# This is where you specify the questions you want included in your worksheet, in the
+# order you want them to appear.
+pdflatex stasjon02.tex
+pdflatex stasjon03.tex
+pdflatex stasjon04.tex
+pdflatex stasjon05.tex
+pdflatex stasjon07.tex
+
+cp stasjon*.pdf ../output/
+

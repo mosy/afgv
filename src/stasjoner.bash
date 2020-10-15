97ce1e0382b46728db08cba1d45fa8c985999eaa
Fred-Olav <fred-olav@mosdal.org>
2020-10-15 12:54:15 +0200

    Gjort ferdig utkast til stasjoner

--- src/stasjoner.bash
+++ src/stasjoner.bash
@@ -5,10 +5,18 @@
 # This is where you specify the questions you want included in your worksheet, in the
 # order you want them to appear.
 pdflatex stasjon02.tex
-pdflatex stasjon03.tex
-pdflatex stasjon04.tex
-pdflatex stasjon05.tex
-pdflatex stasjon07.tex
+cp stasjon02.pdf ../output/
 
-cp stasjon*.pdf ../output/
+pdflatex stasjon03.tex
+cp stasjon03.pdf ../output/
+
+pdflatex stasjon04.tex
+cp stasjon04.pdf ../output/
+
+pdflatex stasjon05.tex
+cp stasjon05.pdf ../output/
+
+pdflatex stasjon07.tex
+cp stasjon07.pdf ../output/
+
 

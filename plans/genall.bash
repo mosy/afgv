3b49831aa046cdfd476c888028b886169b4fff50
Fred-Olav Mosdal <fred-olav@mosdal.org>
2021-06-21 16:54:42 +0200

    jobbet med planer for 21/22

--- plans/genall.bash
+++ plans/genall.bash
@@ -4,10 +4,9 @@
 # "Concatenating" all selected questions/answers/notes into a single TeX file.
 # This is where you specify the questions you want included in your worksheet, in the
 # order you want them to appear.
-pdflatex plan20.tex
-pdflatex plan21.tex
-pdflatex teori20.tex
-pdflatex teori21.tex
-
-cp *.pdf ../output/
-
+pdflatex ./planauntum.tex
+pdflatex ./planspring.tex
+pdfunite planauntum.pdf planspring.pdf temp.pdf
+qpdf temp.pdf --pages . 2-3 -- 3AUA2122.pdf
+cp 3AUA2122.pdf ../output/
+rm *.pdf
